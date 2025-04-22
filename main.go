df20a29b729207deff79bbed0d52b427936be9b4
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-22 11:41:53 +0100

    context with time out

--- main.go
+++ main.go
@@ -1,6 +1,39 @@
 package main
-import ("fmt")
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 func main() {
 
 	fmt.Println("Hello, World!")
+	ctx := context.Background()
+	exampleTimeout(ctx)
+}
+
+func exampleTimeout(ctx context.Context) {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 5*time.Second);
+	defer cancel()
+
+	done:=make(chan struct{})
+
+
+	go func() {
+		time.Sleep(6*time.Second)
+		close(done)
+	}()
+select {
+case <-done:
+	fmt.Println("call the api")
+	case <-ctxWithTimeout.Done():
+		fmt.Println("oh no my timeout expired",ctxWithTimeout.Err())
+		//do some logic here
+}
+}
+
+
+func exampleWithValues(){
+	
 }
