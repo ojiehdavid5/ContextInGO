108f4a440e51c207d11bc4781b3885b3f89ead96
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-22 16:13:51 +0100

    still on timeout

--- main.go
+++ main.go
@@ -10,46 +10,46 @@ import (
 func main() {
 
 	fmt.Println("Hello, World!")
-	// ctx := context.Background()
-	// exampleTimeout(ctx)
+	ctx := context.Background()
+	exampleTimeout(ctx)
 
 	// exampleWithValues()
-	http.HandleFunc("/", helloHandler)
+	// http.HandleFunc("/", helloHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	ctx,cancel:= context.WithTimeout(r.Context(), 3*time.Second)
-	defer cancel()
-	select {
-	case <-time.After(1*time.Second):
-		fmt.Println("Api response")
-		case <-ctx.Done():
-			fmt.Println("oh no my timeout expired",ctx.Err())
-			http.Error(w, "Request timed out", http.StatusRequestTimeout)
-
-}}
-
-// func exampleTimeout(ctx context.Context) {
-// 	ctxWithTimeout, cancel := context.WithTimeout(ctx, 5*time.Second);
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+// 	ctx,cancel:= context.WithTimeout(r.Context(), 3*time.Second)
 // 	defer cancel()
+// 	select {
+// 	case <-time.After(1*time.Second):
+// 		fmt.Println("Api response")
+// 		case <-ctx.Done():
+// 			fmt.Println("oh no my timeout expired",ctx.Err())
+// 			http.Error(w, "Request timed out", http.StatusRequestTimeout)
 
-// 	done:=make(chan struct{})
+// }}
+
+func exampleTimeout(ctx context.Context) {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 5*time.Second);
+	defer cancel()
+
+	done:=make(chan struct{})
 
 
-// 	go func() {
-// 		time.Sleep(6*time.Second)
-// 		close(done)
-// 	}()
-// select {
-// case <-done:
-// 	fmt.Println("call the api")
-// 	case <-ctxWithTimeout.Done():
-// 		fmt.Println("oh no my timeout expired",ctxWithTimeout.Err())
-// 		//do some logic here
-// }
-// }
+	go func() {
+		time.Sleep(1*time.Second)
+		close(done)
+	}()
+select {
+case <-done:
+	fmt.Println("call the api")
+	case <-ctxWithTimeout.Done():
+		fmt.Println("oh no my timeout expired",ctxWithTimeout.Err())
+		//do some logic here
+}
+}
 
 
 // func exampleWithValues(){
